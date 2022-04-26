2baa615648ae8ab557a768a15d4df57692068dd0
unknown <fabio_pachecosilva@hotmail.com>
2022-04-25 23:14:19 -0300

    removendo script antigo e atualizando para o novo

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,63 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+const form = document.forms['form']
+form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(event) {
-  event.preventDefault();
+function handleSubmit(e) {
+    e.preventDefault();
 
-  const weight = getInputNumberValue("weight");
-  const height = getInputNumberValue("height");
-  const age = getInputNumberValue("age");
-  const gender = getSelectedValue("gender");
-  // const activityLevel = getSelectedValue("activity__level");
-  const activityLevel = getSelectedValue("level");
+    const weight = form.weight.value;
+    const height = Math.round(form.height.value);
+    const age = form.age.value;
+    const gender = form.gender.value;
+    const level = form.level.value;    
 
-  const basal = Math.round(
-    gender === 'female'
-      ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
-      : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
-  );
+    gender == 'male' ? basal = (10 * weight) + ( 6.25 * height ) - ( 5 * age ) + 5 : basal = (10 * weight) + ( 6.25 * height ) - ( 5 * age ) -161;
 
-  const maintenance = Math.round(basal * Number(activityLevel));
-  const gainWeight = maintenance + 450;
-  const loseWeight = maintenance - 450;
+    basal = Math.round(basal)
+    basalFinal = Math.round(basal * level);
+    basalAct = Math.round(basalFinal - ( basalFinal * 0.25 ))
 
-  document.getElementById("outBasal");
-  outBasal.innerHTML = "Seu metabolismo basal é de " + basal  + " calorias";;
+    console.log(basal)    
+    console.log(Math.round(basalAct))
+    console.log(basalFinal)
+    
+    var outBasal = document.getElementById("outBasal");
+    // outBasal.innerHTML = "Taxa basal: " + basal + " calorias";
+    outBasal.innerHTML = `Taxa basal: ${basal} calorias`;
+    var outAct = document.getElementById("outAct");
+    // outAct.innerHTML = "Taxa com atividade: " + basalAct + " calorias";
+    outAct.innerHTML = `Taxa com atividade: ${basalAct} calorias`;
+    var outDef = document.getElementById("outDef");
+    // outDef.innerHTML = "Taxa de déficit calórico: " + basalFinal + " calorias";
+    outDef.innerHTML = `Taxa de déficit calórico: ${basalFinal} calorias`;
 
-  document.getElementById("outMaintenance");
-  outMaintenance.innerHTML = "Para manter peso você precisa consumir em média " + maintenance + " calorias" ;
+    const proteinPure = form.protein.value;
+    const protein = Math.round(form.protein.value * weight);    
+    const fatPure = form.fat.value;
+    const fat = Math.round(form.fat.value * weight);    
+    const carbPure = form.carb.value;
+    const carb = Math.round(form.carb.value * weight);
 
-  document.getElementById("outLoseweight");
-  outLoseWeight.innerHTML = "Para perder peso você precisa consumir em média " + loseWeight + " calorias" ;
+    resultForm = document.querySelectorAll('.result__content .form-group')
+    for (let index = 0; index < resultForm.length; index++) {
+        const element = resultForm[index];
+        
+        element.style.display = 'none';
+    }
 
-  document.getElementById("outGainWeight");
-  outGainWeight.innerHTML = "Para ganhar peso você precisa consumir em média " + gainWeight + " calorias" ;
+    document.getElementById("outProtein");
+    outProtein.innerHTML = `Proteínas ( ${proteinPure}g ): ${protein}g`;
+    document.getElementById("outFat");
+    outFat.innerHTML = `Gorduras ( ${fatPure}g ): ${fat}g`;
+    document.getElementById("outCarb");
+    outCarb.innerHTML = `Carbos ( ${carbPure}g ): ${carb}g`;
+    
 }
 
-function getSelectedValue(id) {
-  const select = document.getElementById(id);
-  return select.options[select.selectedIndex].value;
-}
-
-function getInputNumberValue(id) {
-  return Number(document.getElementById(id).value);
-}
+// function getSelectedValue(id) {
+//     const select = document.getElementById(id);
+//     return select.options[select.selectedIndex].value;
+// }
+  
+// function getInputNumberValue(id) {
+//     return Number(document.getElementById(id).value);
+// }
