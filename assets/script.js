35011c799fe885775941fcd9835280d74bc2d5bb
unknown <fabio_pachecosilva@hotmail.com>
2022-06-07 22:11:20 -0300

    corrigindo erro do height

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,10 @@ function handleSubmit(e) {
     e.preventDefault();
 
     const weight = form.weight.value;
-    const height = Math.round(form.height.value);
+    const height = form.height.value.replace('.','');
     const age = form.age.value;
     const gender = form.gender.value;
-    const level = form.level.value;    
+    const level = form.level.value;
 
     gender == 'male' ? basal = (10 * weight) + ( 6.25 * height ) - ( 5 * age ) + 5 : basal = (10 * weight) + ( 6.25 * height ) - ( 5 * age ) -161;
 
@@ -23,12 +23,13 @@ function handleSubmit(e) {
     outAct.innerHTML = `Taxa com atividade: ${basalAct} calorias`;
     outDef.innerHTML = `Taxa de déficit calórico: ${basalFinal} calorias`;
 
+
     const proteinPure = form.protein.value;
-    const protein = Math.round(form.protein.value * weight);    
+    const protein = Math.round(form.protein.value * weight);
     const fatPure = form.fat.value;
     const fat = Math.round(form.fat.value * weight);    
     const carbPure = form.carb.value;
-    const carb = Math.round(form.carb.value * weight);    
+    const carb = Math.round(form.carb.value * weight);
 
     var outProtein = document.getElementById("outProtein");
     var outFat = document.getElementById("outFat");
@@ -36,8 +37,9 @@ function handleSubmit(e) {
     outProtein.innerHTML = `Proteínas ( ${proteinPure}g ): ${protein}g`;
     outFat.innerHTML = `Gorduras ( ${fatPure}g ): ${fat}g`;
     outCarb.innerHTML = `Carbos ( ${carbPure}g ): ${carb}g`;
+    
+    
 
-    // escondeForm();
     mostraBadges();
 }
 
@@ -51,8 +53,6 @@ function escondeForm() {
         const element = resultForm[index];        
         element.style.display = 'none';
     }
-    // resetButton.style.display = 'inline-block';
-    // resultButton.style.display = 'none';
 }
 
 function mostraForm() {
@@ -60,37 +60,12 @@ function mostraForm() {
         const element = resultForm[index];        
         element.style.display = 'block';
     }
-    // resetButton.style.display = 'none';
-    // resultButton.style.display = 'inline-block';    
 }
-
-// function escondeBadges() {
-//     for (let index = 0; index < badges.length; index++) {
-//         const badge = badges[index];        
-//         // badge.style.display = 'none';
-//     }
-//     // document.querySelector('.rowBadges').style.display = 'none';
-//     outBasal.innerHTML = '';
-//     outAct.innerHTML = '';
-//     outDef.innerHTML = '';
-// }
 
 function mostraBadges() {
     for (let index = 0; index < badges.length; index++) {
         const badge = badges[index];        
         badge.style.display = 'inline-block';
     }
-    document.querySelector('.rowBadges').style.display = 'flex';
+    document.querySelector('.rowBadges').style.display = 'flex';    
 }
-
-// function validaReset() {
-//     var proteinButton = document.querySelector('#outProtein')
-//     var res = window.getComputedStyle(proteinButton, null).display;
-
-//     escondeForm();
-//     escondeBadges();
-
-//     if(res !== 'none') {
-//         mostraForm();
-//     } 
-// }
